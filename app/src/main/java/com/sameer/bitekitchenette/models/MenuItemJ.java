aef6c6b439aa18fefc6258aec83edcacc0981041
mhashir1010 <m.hashir10102@gmail.com>
2022-01-26 14:54:57 +0500

    adding models menu items and pack size

--- app/src/main/java/com/sameer/bitekitchenette/models/MenuItemJ.java
+++ app/src/main/java/com/sameer/bitekitchenette/models/MenuItemJ.java
@@ -4,6 +4,40 @@ import java.util.ArrayList;
 
 public class MenuItemJ {
 
-   
+    public int id = -1;
+    public String itemName;
+    public ArrayList<PackSizeJ> packSizes = new ArrayList();
+    public boolean deal = false;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public ArrayList<PackSizeJ> getPackSizes() {
+        return packSizes;
+    }
+
+    public void setPackSizes(ArrayList<PackSizeJ> packSizes) {
+        this.packSizes = packSizes;
+    }
+
+    public boolean getDeal() {
+        return deal;
+    }
+
+    public void setDeal(boolean deal) {
+        this.deal = deal;
     }
 }
--- app/src/main/java/com/sameer/bitekitchenette/models/PackSizeJ.java
+++ app/src/main/java/com/sameer/bitekitchenette/models/PackSizeJ.java
@@ -2,4 +2,58 @@ package com.sameer.bitekitchenette.models;
 
 public class PackSizeJ {
 
+    public int id = -1;
+    public String name;
+    public int packSize;
+    public int price = -1;
+    public int free = -1;
+    public int quantity = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPackSize() {
+        return packSize;
+    }
+
+    public void setPackSize(int packSize) {
+        this.packSize = packSize;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getFree() {
+        return free;
+    }
+
+    public void setFree(int free) {
+        this.free = free;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
