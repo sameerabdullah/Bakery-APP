6a4d3a054cf7d5a3c4ac72cd97baecbeb4a73ad4
mhashir1010 <m.hashir10102@gmail.com>
2022-01-26 15:00:07 +0500

    adding viewholders

--- app/src/main/java/com/sameer/bitekitchenette/viewHolders/MenuListViewJHolder.java
+++ app/src/main/java/com/sameer/bitekitchenette/viewHolders/MenuListViewJHolder.java
@@ -1,10 +1,36 @@
 package com.sameer.bitekitchenette.viewHolders;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.sameer.bitekitchenette.R;
 
 public class MenuListViewJHolder extends RecyclerView.ViewHolder {
 
-   
+    public CardView menuCard;
+    public ConstraintLayout menuItem;
+    public TextView itemName, price, quantity;
+    public ImageView increaseQuantity, decreaseQuantity, cart;
+    public RecyclerView packSizeList;
+    public Button addToCart;
+
+    public MenuListViewJHolder(View itemView){
+        super(itemView);
+        menuCard = itemView.findViewById(R.id.menu_card);
+        menuItem = itemView.findViewById(R.id.menu_item);
+        itemName = itemView.findViewById(R.id.item_name);
+        packSizeList = itemView.findViewById(R.id.pack_sizes);
+        addToCart = itemView.findViewById(R.id.add_to_cart);
+        price = itemView.findViewById(R.id.price);
+        quantity = itemView.findViewById(R.id.quantity);
+        increaseQuantity = itemView.findViewById(R.id.increase_quantity);
+        decreaseQuantity = itemView.findViewById(R.id.decrease_quantity);
+        cart = itemView.findViewById(R.id.cart);
+    }
 }
--- app/src/main/java/com/sameer/bitekitchenette/viewHolders/PackSizeListViewJHolder.java
+++ app/src/main/java/com/sameer/bitekitchenette/viewHolders/PackSizeListViewJHolder.java
@@ -1,7 +1,31 @@
 package com.sameer.bitekitchenette.viewHolders;
 
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.sameer.bitekitchenette.R;
 
 public class PackSizeListViewJHolder extends RecyclerView.ViewHolder {
 
+    public ConstraintLayout sizeItem;
+    public TextView size, free, price, quantity;
+    public ImageView increaseQuantity, decreaseQuantity, cart;
+
+    public PackSizeListViewJHolder(@NonNull View itemView) {
+        super(itemView);
+
+        sizeItem = itemView.findViewById(R.id.pack_size_item);
+        size = itemView.findViewById(R.id.size);
+        free  = itemView.findViewById(R.id.free);
+        price = itemView.findViewById(R.id.price);
+        quantity = itemView.findViewById(R.id.quantity);
+        increaseQuantity = itemView.findViewById(R.id.increase_quantity);
+        decreaseQuantity = itemView.findViewById(R.id.decrease_quantity);
+        cart = itemView.findViewById(R.id.cart);
+    }
 }
